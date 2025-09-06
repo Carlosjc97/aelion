9fa7636409f81e95a8e52b300dd9dcec11c952fa
Jean Carlos <jvillaltavillamar@gmail.com>
2025-09-06 08:53:27 -0500

    Actualizar home_smoke_test.dart

--- test/home_smoke_test.dart
+++ test/home_smoke_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:learning_ia/features/home/home_view.dart';
 import 'package:learning_ia/widgets/course_card.dart';
 
@@ -8,15 +7,18 @@ void main() {
   testWidgets('HomeView smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: HomeView()));
 
-    // Deben renderizarse exactamente 2 CourseCard en el carrusel
+    // Ahora tenemos 2 cards visibles en HomeView
     expect(find.byType(CourseCard), findsNWidgets(2));
 
-    // Semantics de accesibilidad
+    // Verificamos accesibilidad
     expect(find.bySemanticsLabel("Open 'Toma un curso'"), findsOneWidget);
     expect(find.bySemanticsLabel("Open 'Aprende un idioma'"), findsOneWidget);
 
-    // Sección de populares
+    // Sección de cursos populares
     expect(find.text('Cursos populares'), findsOneWidget);
     expect(find.text('Introducción a la IA'), findsOneWidget);
+
+    // Verificar que "Aprende un idioma" aparece
+    expect(find.text('Aprende un idioma'), findsOneWidget);
   });
 }
